c0f28c1734ee496464bad1702bad714268fda660
Filip Molcik <fmolcik@gmail.com>
2019-11-19 10:39:02 +0100

    add dotenv add .env.example with example tokens

--- services/client.js
+++ services/client.js
@@ -1,7 +1,3 @@
-const key = '88722d7cc4a4afd768af046f96bfa1bb';
-const secret = 'ZIY4+6QKfFTKeOXdq9rsxOj/pahK+QFy49gWzPtbOZYoOV5eNv4rwSbstmzcvg41DCzutVf8nhwJjNztPaJEkw==';
-const passphrase = 'mz89z1rpdwd';
-
 const apiURI = 'https://api.pro.coinbase.com';
 const sandboxURI = 'https://api-public.sandbox.pro.coinbase.com';
 
